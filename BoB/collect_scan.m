5f301571d7eeca60ef8099fb3a8485fdcecd9983
rowan-jansens <rowan.jansens@gmail.com>
2022-05-10 10:33:45 -0400

    Final commit

--- BoB/collect_scan.m
+++ BoB/collect_scan.m
@@ -1,4 +1,5 @@
 function [x, y] = collect_scan()
+%collect a lidar scan, clean the data, and convert to cartesian
 sub = rossubscriber('/scan');
 scan_message = receive(sub);
 r = scan_message.Ranges(1:end-1);
@@ -7,6 +8,4 @@ index=find(r~=0);
 r_clean=r(index);
 theta_clean=theta(index);
 [x,y]=pol2cart(theta_clean,r_clean);
-
-
 end
