43b284be38ef059682cf287d1c08c22cde213a7a
Madeline-Robertson <98299484+Madeline-Robertson@users.noreply.github.com>
2022-05-09 23:36:13 -0400

    qwertyui

--- BoB/PathPlotter.m
+++ BoB/PathPlotter.m
@@ -1,19 +1,23 @@
 %load my_data.csv
+clf
 load potential.mat;
 load outliers.mat;
 load inliers.mat;
 load center.mat;
-known_radius = 0.1329;
+load X.mat;
+load Y.mat;
+points = [inliers; outliers];
+known_radius = 0.25;
 data = readtable('my_data.csv');
 rt = encoder_witchcraft(data, 105, 800);
 hold on
-plot(rt(:, 1), rt(:,2), 'k--')
-plot(path(:,1), path(:,2))
+plot(rt(:, 1), rt(:,2), 'k--', 'linewidth', 2)
+plot(path(:,1), path(:,2), 'linewidth', 2)
 xlabel('X Position (m)')
 ylabel('Y Position (m)')
 title('Theoretical vs Measured Neato Path')
 grid on
-legend('1', '2', '3', '4', '5','6','7')
+
 th = 0:pi/50:2*pi;
 xunit = known_radius * cos(th) + center(1);
 yunit = known_radius * sin(th) + center(2);
@@ -24,7 +28,8 @@ contour(X, Y, real(potential),30)
 %quiver(X, Y, dx, dy, 2, "color", [.3 0.7 .3])
 
 scatter(0,0,100, 'sb',  "filled")
-quiver(0, 0, ascent_vec(1), ascent_vec(2), .01)
+%quiver(0, 0, ascent_vec(1), ascent_vec(2), .01)
 
-axis([-2 2 -2 2])
+axis([-1.5 3.5 -3 1])
+legend('Theoretical Path', 'Measured Path', '', '', '', '', 'Neato')
 hold off
